e1b3d819bf1c6aa24aecba47c38e53a12e424e69
Anders Solberg <ssredna@gmail.com>
2024-02-10 16:51:41 +0100

    add basic screening schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import author from './author'
 import image_assets from './image_assets'
 import review from './review'
+import screening from './screening'
 
-export const schemaTypes = [author, review, image_assets]
+export const schemaTypes = [author, review, image_assets, screening]
---
+++ schemas/screening.ts
@@ -0,0 +1,46 @@
+import {Rule, SanityDocument} from 'sanity'
+
+export default {
+  name: 'screening',
+  title: 'Visning',
+  type: 'document',
+  fields: [
+    {
+      name: 'movie_title',
+      title: 'Filmtittel',
+      type: 'string',
+    },
+    {
+      name: 'release_year',
+      title: 'Utgivelsesår',
+      type: 'number',
+      validation: (Rule: Rule) => Rule.required().integer().positive(),
+    },
+    {
+      name: 'director',
+      title: 'Regissør',
+      type: 'string',
+    },
+    {
+      name: 'date',
+      title: 'Visningsdato',
+      type: 'date',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: (doc: SanityDocument) => doc.date + '-' + doc.movie_title,
+      },
+    },
+    {
+      name: 'poster',
+      title: 'Poster',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
