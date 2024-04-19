5d16abaf3c94c794ff0cb818594e41ebccd88da3
Anders Solberg <ssredna@gmail.com>
2024-04-19 21:29:09 +0200

    add schema for 'about us' page

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,8 @@
 import author from './author'
 import image_assets from './image_assets'
+import page_om_oss from './page_om_oss'
 import review from './review'
 import screening from './screening'
 import semester from './semester'
 
-export const schemaTypes = [author, review, image_assets, screening, semester]
+export const schemaTypes = [author, review, image_assets, screening, semester, page_om_oss]
---
+++ schemas/page_om_oss.ts
@@ -0,0 +1,38 @@
+import {defineType} from 'sanity'
+
+export default defineType({
+  name: 'page_om_oss',
+  title: 'Side: Om oss',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Side-innhold til "Om oss"',
+      type: 'string',
+      description: 'Denne kan også bare være film-tittelen',
+    },
+    {
+      name: 'header_image',
+      title: 'Hoved-bilde',
+      type: 'object',
+      fields: [
+        {
+          name: 'image',
+          title: 'Bilde',
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          validation: (rule) => rule.required(),
+        },
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternativ tekst',
+          description: 'Beskriv bildet for skjermlesere',
+          validation: (rule) => rule.required(),
+        },
+      ],
+    },
+  ],
+})
