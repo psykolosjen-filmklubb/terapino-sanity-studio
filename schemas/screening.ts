a5b55736df1dd051eff1ea878deeee049361d85c
Quentin Terapino <filmklubb.psykolosjen@gmail.com>
2025-03-11 16:21:16 +0100

    use event_media type for promo_material and event_media in screenings

--- schemas/screening.ts
+++ schemas/screening.ts
@@ -66,72 +66,12 @@ export default defineType({
     defineField({
       name: 'promo_material',
       title: 'Promomateriale',
-      type: 'array',
-      of: [
-        defineArrayMember({
-          name: 'image',
-          type: 'image',
-          title: 'Bilde',
-          fields: [
-            defineField({
-              name: 'alt',
-              type: 'string',
-              title: 'Alternativ tekst',
-              description: 'Beskriv bildet for skjermlesere',
-              validation: (rule) => rule.required(),
-            }),
-          ],
-        }),
-        defineArrayMember({
-          name: 'video',
-          type: 'object',
-          title: 'Video',
-          fields: [
-            defineField({
-              name: 'youtube_id',
-              type: 'string',
-              title: 'YouTube ID',
-              description: (
-                <span>
-                  Legg inn ID-en til YouTube-videoen. F.eks. i{' '}
-                  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-                    youtube.com/watch?v=dQw4w9WgXcQ
-                  </a>
-                  , så er &quot;dQw4w9WgXcQ&quot; ID-en.
-                </span>
-              ),
-              validation: (rule) => rule.required(),
-            }),
-          ],
-        }),
-      ],
-      options: {
-        layout: 'grid',
-      },
+      type: 'event_media',
     }),
     defineField({
       name: 'event_media',
       title: 'Bilder fra visningen',
-      type: 'array',
-      of: [
-        defineArrayMember({
-          name: 'image',
-          type: 'image',
-          title: 'Bilde',
-          fields: [
-            defineField({
-              name: 'alt',
-              type: 'string',
-              title: 'Alternativ tekst',
-              description: 'Beskriv bildet for skjermlesere',
-              validation: (rule) => rule.required(),
-            }),
-          ],
-        }),
-      ],
-      options: {
-        layout: 'grid',
-      },
+      type: 'event_media',
     }),
   ],
   orderings: [
